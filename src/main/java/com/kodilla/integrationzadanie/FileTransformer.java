185e6445ec5853ab3c83552efe6a7d0111a6cc1e
unknown <kwaswi@wp.pl>
2020-08-20 23:07:34 +0200

    first commit

---
+++ src/main/java/com/kodilla/integrationzadanie/FileTransformer.java
@@ -0,0 +1,15 @@
+package com.kodilla.integrationzadanie;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileTransformer {
+
+    public String transformFile(String fileName) throws IOException {
+
+        return "";
+    }
+
+}
---
+++ src/main/java/com/kodilla/integrationzadanie/IntegrationZadanieApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.integrationzadanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationZadanieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationZadanieApplication.class, args);
+    }
+
+}
